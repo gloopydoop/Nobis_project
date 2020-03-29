04d465242b40ce325802f08b9c7155c37f54dc84
Harrison Nobis <nobis@mech.kth.se>
2020-03-29 12:26:52 +0200

    big diggidy dawg

--- Rayleigh_Bernard.m
+++ Rayleigh_Bernard.m
@@ -17,42 +17,42 @@ param.ig = 9999999;      % number of iterations between output
 
 
 
-%% case A
-% Boundary and initial conditions:
+% %% case A
+% % Boundary and initial conditions:
 param.Utop = 0.; 
 param.Ubottom = 0.;
 param.namp = 0.1;
 param.Tbottom = 0.; 
 param.Ttop = 1.;
 param.Tf = 20;       % final time
-% 1)
-param.caseName = 'Part2_Case_A1';
-param.dt = 0.0005;      % time step
-param.Ra = 200;      % Rayleigh number     
-param.Re = 1./param.Pr;    % Reynolds number
-param.Ri = param.Ra*param.Pr;    % Richardson number
-param.Pe = param.Re*param.Pr; %Peclet number
-
-[P2_CA1.maxU,P2_CA1.maxV,P2_CA1.tser,P2_CA1.Tser,P2_CA1.centerU,P2_CA1.centerV,P2_CA1.centerT] = SG2212_template(param);
-% 2)
-param.caseName = 'Part2_Case_A2';
-param.Ra = 2000;      % Rayleigh number     
-param.Re = 1./param.Pr;    % Reynolds number
-param.Ri = param.Ra*param.Pr;    % Richardson number
-param.Pe = param.Re*param.Pr; %Peclet number
-
-[P2_CA2.maxU,P2_CA2.maxV,P2_CA2.tser,P2_CA2.Tser,P2_CA2.centerU,P2_CA2.centerV,P2_CA2.centerT] = SG2212_template(param);
-% 3)
-param.caseName = 'Part2_Case_A3';
-param.Ra = 60000;      % Rayleigh number  
-param.Re = 1./param.Pr;    % Reynolds number
-param.Ri = param.Ra*param.Pr;    % Richardson number
-param.Pe = param.Re*param.Pr; %Peclet number
-
-[P2_CA3.maxU,P2_CA3.maxV,P2_CA3.tser,P2_CA3.Tser,P2_CA3.centerU,P2_CA3.centerV,P2_CA3.centerT] = SG2212_template(param);
-
-
-save('Case2A.mat','P2_CA1','P2_CA2','P2_CA3')
+% % 1)
+% param.caseName = 'Part2_Case_A1';
+% param.dt = 0.0005;      % time step
+% param.Ra = 200;      % Rayleigh number     
+% param.Re = 1./param.Pr;    % Reynolds number
+% param.Ri = param.Ra*param.Pr;    % Richardson number
+% param.Pe = param.Re*param.Pr; %Peclet number
+% 
+% [P2_CA1.maxU,P2_CA1.maxV,P2_CA1.tser,P2_CA1.Tser,P2_CA1.centerU,P2_CA1.centerV,P2_CA1.centerT] = SG2212_template(param);
+% % 2)
+% param.caseName = 'Part2_Case_A2';
+% param.Ra = 2000;      % Rayleigh number     
+% param.Re = 1./param.Pr;    % Reynolds number
+% param.Ri = param.Ra*param.Pr;    % Richardson number
+% param.Pe = param.Re*param.Pr; %Peclet number
+% 
+% [P2_CA2.maxU,P2_CA2.maxV,P2_CA2.tser,P2_CA2.Tser,P2_CA2.centerU,P2_CA2.centerV,P2_CA2.centerT] = SG2212_template(param);
+% % 3)
+% param.caseName = 'Part2_Case_A3';
+% param.Ra = 60000;      % Rayleigh number  
+% param.Re = 1./param.Pr;    % Reynolds number
+% param.Ri = param.Ra*param.Pr;    % Richardson number
+% param.Pe = param.Re*param.Pr; %Peclet number
+% 
+% [P2_CA3.maxU,P2_CA3.maxV,P2_CA3.tser,P2_CA3.Tser,P2_CA3.centerU,P2_CA3.centerV,P2_CA3.centerT] = SG2212_template(param);
+% 
+% 
+% save('Case2A.mat','P2_CA1','P2_CA2','P2_CA3')
 
 %% case B
 % Boundary and initial conditions:
@@ -95,14 +95,14 @@ param.dt = 0.0005;      % time step
 % 
 % [P2_CB3.maxU,P2_CB3.maxV,P2_CB3.tser,P2_CB3.Tser,P2_CB3.centerU,P2_CB3.centerV,P2_CB3.centerT] = SG2212_template(param);
 
-% % 4)
-% param.caseName = 'Part2_Case_B4';
-% param.Ra = 5000;      % Rayleigh number     
-% param.Re = 1./param.Pr;    % Reynolds number
-% param.Ri = param.Ra*param.Pr;    % Richardson number
-% param.Pe = param.Re*param.Pr; %Peclet number
-% 
-% [P2_CB4.maxU,P2_CB4.maxV,P2_CB4.tser,P2_CB4.Tser,P2_CB4.centerU,P2_CB4.centerV,P2_CB4.centerT] = SG2212_template(param);
+% 4)
+param.caseName = 'Part2_Case_B4';
+param.Ra = 5000;      % Rayleigh number     
+param.Re = 1./param.Pr;    % Reynolds number
+param.Ri = param.Ra*param.Pr;    % Richardson number
+param.Pe = param.Re*param.Pr; %Peclet number
+
+[P2_CB4.maxU,P2_CB4.maxV,P2_CB4.tser,P2_CB4.Tser,P2_CB4.centerU,P2_CB4.centerV,P2_CB4.centerT] = SG2212_template(param);
 
 % 4)
 % param.caseName = 'Part2_Case_B4';
